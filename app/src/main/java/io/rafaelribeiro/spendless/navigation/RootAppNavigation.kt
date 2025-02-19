2cc274a131da086244682b0d2869a56de61c2c24
orcunsevsay <orcunsevsay@gmail.com>
2025-02-19 23:18:57 +0100

    RB-32 - Main `androidx.navigation:navigation-compose` library is integrated along with hilt-navigation-compose. So that we can integrate navigation animations. - Also, some libraries updated!

--- app/src/main/java/io/rafaelribeiro/spendless/navigation/RootAppNavigation.kt
+++ app/src/main/java/io/rafaelribeiro/spendless/navigation/RootAppNavigation.kt
@@ -1,8 +1,5 @@
 package io.rafaelribeiro.spendless.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
